d9cf560a4f59f50a702cba6fd5a253d702a93978
amltoken <amltoken@protonmail.com>
2017-09-30 17:42:51 +0800

    Add cmd/amlcoin.go

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ echo "skycoin binary dir:" "$DIR"
 
 pushd "$DIR" >/dev/null
 
-go run cmd/skycoin/skycoin.go --gui-dir="${DIR}/src/gui/static/" $@
+go run cmd/amlcoin/amlcoin.go --gui-dir="${DIR}/src/gui/static/" $@
 
 popd >/dev/null
